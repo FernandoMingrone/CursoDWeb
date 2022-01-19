238518a2114a95ec1bb5ad01e737785a55b30bbc
FernandoMingrone <fernandomingrone@gmail.com>
2022-01-18 22:19:57 -0300

    Tercera entrega Proyecto Final

--- ProyectoFinal/script.js
+++ ProyectoFinal/script.js
@@ -1,44 +1,45 @@
-// let darkMode;
+let darkMode;
 
-// if(localStorage.getItem("darkMode")) {
-//     darkMode = localStorage.getItem("darkMode")
-// } else {
-//     darkMode = "light"
-// }
+if(localStorage.getItem("darkMode")) {
+    darkMode = localStorage.getItem("darkMode")
+} else {
+    darkMode = "light"
+}
 
-// localStorage.setItem("darkMode", darkMode)
+localStorage.setItem("darkMode", darkMode)
 
-// $(() => {
-//     if(localStorage.getItem("darkMode") == "dark") {
-//         $("#nav").addClass("darkMode")
-//         $("#section").addClass("darkMode2")
-//         $("#footer").addClass("darkMode2")
-//         $("#propiedades").addClass("darkMode")
-//         $("#divMostrarPropiedades").addClass("darkModeCard")
-//         $("#btnDark").hide()
-//         $("#btnLight").show()
-//     } else {
-//         $("#btnLight").hide()
-//     };
+$(() => {
+    if(localStorage.getItem("darkMode") == "dark") {
+        $("#header").addClass("darkMode")
+        $("#projects").addClass("darkMode2")
+        $("#technologies").addClass("darkMode")
+        $("#footer").addClass("darkMode2")
+        $("#projects_cards").addClass("darkModeCard")
+        $("#btnDark").hide()
+        $("#btnLight").show()
+    } else {
+        $("#btnLight").hide()
+    };
 
-//     $("#btnLight").click(() => {
-//         $("#btnDark").show()
-//         $("#btnLight").hide() 
-//         $("#nav").removeClass("darkMode")
-//         $("#section").removeClass("darkMode2")
-//         $("#footer").removeClass("darkMode2")
-//         $("#propiedades").removeClass("darkMode")
-//         localStorage.setItem("darkMode", "light")
-//     })
+    $("#btnLight").click(() => {
+        $("#btnDark").show()
+        $("#btnLight").hide() 
+        $("#header").removeClass("darkMode")
+        $("#projects").removeClass("darkMode2")
+        $("#technologies").removeClass("darkMode")
+        $("#footer").removeClass("darkMode2")
+        $("#projects_cards").removeClass("darkModeCard")
+        localStorage.setItem("darkMode", "light")
+    })
 
-//         $("#btnDark").click(() => {
-//         $("#btnDark").hide()
-//         $("#btnLight").show() 
-//         $("#nav").addClass("darkMode")
-//         $("#section").addClass("darkMode2")
-//         $("#footer").addClass("darkMode2")
-//         $("#propiedades").addClass("darkMode")
-//         $("#divMostrarPropiedades").addClass("darkModeCard")
-//         localStorage.setItem("darkMode", "dark")
-//     })
-// })
+    $("#btnDark").click(() => {
+        $("#btnDark").hide()
+        $("#btnLight").show() 
+        $("#header").addClass("darkMode")
+        $("#projects").addClass("darkMode2")
+        $("#technologies").addClass("darkMode")
+        $("#footer").addClass("darkMode2")
+        $("#projects_cards").addClass("darkModeCard")
+        localStorage.setItem("darkMode", "dark")
+    })
+})
